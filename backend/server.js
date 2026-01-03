ef6c5483b2f844d2b9fec865c4e18b2f801720a8
Ahmed Tanwar <ahmedtanwar017@gmail.com>
2026-01-03 18:51:56 +0530

    Fix CORS for Netlify frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 // app.use(cors({ origin: "http://localhost:3000" }));
-app.use(cors({ origin: "https://task-tracker-6blo.onrender.com" }));
+app.use(cors({ origin: "https://tasktrackerss.netlify.app" }));
 app.use(express.json());
 
 // Routes
